f7f73e663d287b88051f880fa2785e57d7703c54
oluwabukola <daniellabaiyewu@gmail.com>
2020-03-11 22:48:16 +0100

    genre

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+const Joi = require('joi');
+
+const genres = require('./routes/genres');
+
+const customers = require('./routes/customers');
+
+const movies = require('./routes/movies');
+
+const express = require('express');
+
+const app = express();
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('connected to MongoDb...'))
+    .catch(err => console.error('could not connect to MongoDb...'));
+
+app.use(express.json());
+
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+
+const port = process.env.PORT || 3000;
+
+app.listen (port, () => {
+    console.log(`Listening on port ${port}...`);
+});
